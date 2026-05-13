cbfc43c93dfe3f9f83855bf673b1871afea6dded
Martin Lee <martinlee5159@gmail.com>
2026-05-12 19:31:06 -0700

    add tasks for megan, onkar, nithin

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// TODO: Onkar add /about. This displays app information such as name, port, and description.
+
+// TODO: Megan add /teams route. This will display all team member names.
+
+// TODO: Nithing add 404 handler. This will handle 404 not found requests.
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
