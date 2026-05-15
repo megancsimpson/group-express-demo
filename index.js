e03b107dd2f43784b28a9094e0fcc8fad9f6be0c
Your Full Name <j.clifford32@hotmail.co.uk>
2026-05-14 20:26:31 -0700

    Update index.js with my changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+app.set('view engine', 'ejs');
 const PORT = 3000;
 
 app.use(express.json());
